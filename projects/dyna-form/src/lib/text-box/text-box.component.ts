27b80b5fec8ccd470fac44cdf3d8e2e564e4b3c2
yanpred <yaniv@praedicta.com>
2019-08-28 14:15:26 +0300

    type input

--- projects/dyna-form/src/lib/text-box/text-box.component.ts
+++ projects/dyna-form/src/lib/text-box/text-box.component.ts
@@ -3,24 +3,32 @@ import { FormControl } from '@angular/forms';
 import { MyErrorStateMatcher } from './MyErrorStateMatcher';
 
 @Component({
-  selector: 'p-text-box',
-  template: `
-<form *ngIf="form" [formGroup]="form.parent">
-<mat-form-field [appearance]="appearance">
-  <input matInput [placeholder]="placeholder" [formControl]="form" [name]="name" [formControlName]="name"
-    [errorStateMatcher]="matcher">
-  <mat-hint *ngIf='hint'>{{hint}}</mat-hint>
-  <mat-error *ngIf="form.invalid"> {{"validations." + matcher.error}} </mat-error>
-</mat-form-field>
-</form>
-`,
-  styles: ['mat-form-field{width: 100%;}']
+    selector: 'p-text-box',
+    template: `
+        <form *ngIf="form" [formGroup]="form.parent">
+            <mat-form-field [appearance]="appearance">
+                <input
+                    [type]="type"
+                    matInput
+                    [placeholder]="placeholder"
+                    [formControl]="form"
+                    [name]="name"
+                    [formControlName]="name"
+                    [errorStateMatcher]="matcher"
+                />
+                <mat-hint *ngIf="hint">{{ hint }}</mat-hint>
+                <mat-error *ngIf="form.invalid"> {{ matcher.error }} </mat-error>
+            </mat-form-field>
+        </form>
+    `,
+    styles: ['mat-form-field{width: 100%;}'],
 })
 export class TextBoxComponent {
-  @Input() form: FormControl;
-  @Input() name: string;
-  @Input() placeholder = '';
-  @Input() hint;
-  @Input() appearance = 'outline';
-  matcher = new MyErrorStateMatcher();
+    @Input() form: FormControl;
+    @Input() name: string;
+    @Input() placeholder = '';
+    @Input() hint;
+    @Input() type = 'text';
+    @Input() appearance = 'outline';
+    matcher = new MyErrorStateMatcher();
 }
